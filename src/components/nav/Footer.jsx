f014b08189fdf1a2d7bbd1f1dd7bffc6dde6f111
caitlinanderson099 <caitlin.anderson099@gmail.com>
2024-07-29 22:20:17 +1200

    styled the footer of the site

--- src/components/nav/Footer.jsx
+++ src/components/nav/Footer.jsx
@@ -3,7 +3,34 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer>
-      <h2> Copyright Caitlin Anderson 2024 </h2>
+
+      <div id='left-side'>
+        <h2> Get In Touch </h2>
+        <div className='contact-container'>
+          
+          <div className='contact-info'>
+            <h4> X </h4>
+            <p> caitlin.anderson099@gmail.com </p>
+          </div>
+
+          <div className='contact-info'>
+          <h4> X </h4>
+          <p> 027 341 2624 </p>
+          </div>
+
+          <div className='contact-info'>
+          <h4> X </h4>
+          <p> Caitlin Anderson </p>
+          </div>
+        </div>
+      </div>
+    
+
+      <div id='right-side'>
+        <img src='/img/site-img/portfolio-logo-transparent.png' alt='portfolio logo' id='footer-logo'/>
+        <h4> Copyright Caitlin Anderson 2024 </h4>
+      </div>
+
     </footer>
   )
 }
